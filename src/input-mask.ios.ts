724aefe9030fc6f2d0b35941e86aaff5cd3dc8e8
Vince Cooley <vince.r.cooley@gmail.com>
2017-09-05 21:47:57 -0500

    working delegate

--- src/input-mask.ios.ts
+++ src/input-mask.ios.ts
@@ -14,42 +14,33 @@ export class InputMaskDelegateImpl extends MaskedTextFieldDelegate {
 	private _defaultImpl: UITextFieldDelegate;
 
 	public textFieldShouldBeginEditing(textField: UITextField): boolean {
-		console.log('should begin')
-
 		return this._defaultImpl.textFieldShouldBeginEditing(textField);
 	}
 
 	public textFieldDidBeginEditing(textField: UITextField): boolean {
-		console.log('did begin')
-
 		return this._defaultImpl.textFieldShouldBeginEditing(textField);
 	}
 
 	public textFieldDidEndEditing(textField: UITextField) {
-		console.log('did end')
-
 		return this._defaultImpl.textFieldDidEndEditing(textField);
 	}
 
 	public textFieldShouldClear(textField: UITextField) {
-		console.log('should clear')
-
 		return this._defaultImpl.textFieldShouldClear(textField);
 	}
 
 	public textFieldShouldReturn(textField: UITextField): boolean {
-		console.log('should return')
-
 		return this._defaultImpl.textFieldShouldReturn(textField);
 	}
 
 	public textFieldShouldChangeCharactersInRangeReplacementString(textField: UITextField, range: NSRange, replacementString: string): boolean {
-		console.log('should change')
-		const shouldChange = super.textFieldShouldChangeCharactersInRangeReplacementString(textField, range, replacementString);
-		if (shouldChange) {
+		const previousText = textField.text;
+		super.textFieldShouldChangeCharactersInRangeReplacementString(textField, range, replacementString);
+		const newText = textField.text;
+		if (previousText !== newText) {
 			this._defaultImpl.textFieldShouldChangeCharactersInRangeReplacementString(textField, range, replacementString);
 		}
-		return true;
+		return false;
 	}
 }
 
@@ -59,7 +50,6 @@ export class InputMask extends InputMaskBase {
 	constructor() {
 		super();
 		const owner = new WeakRef(this);
-		console.log('laskdfj')
 		this._delegate = <InputMaskDelegateImpl>InputMaskDelegateImpl.initWithOwnerAndDefault(owner, this._delegate);
 	}
 
